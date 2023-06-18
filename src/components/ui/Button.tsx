d546a6bb33c524a56ac4b5ea85c74b0ae5940ad9
rpavlini <robert.pavlinic95@gmail.com>
2023-06-18 23:55:33 +0200

    button hover tweaks

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,8 +2,6 @@ import clsx from 'clsx';
 import { ComponentProps, ReactNode, forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-import './Button.css';
-
 type Props = {
   iconLeft?: ReactNode;
   iconRight?: ReactNode;
@@ -30,28 +28,34 @@ const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ComponentProps<
     const Component = as;
 
     return (
-      <Component
-        ref={ref as any}
-        {...(rest as any)}
-        className={twMerge(
-          clsx(
-            'transition-all duration-200  ui-button rounded-full flex items-center gap-3 focus-visible:outline-none relative',
-            className || '',
-            {
-              ['text-sm leading-6 py-[6px] px-4']: size === 'default',
-              ['text-xs px-3 py-1']: size === 'sm',
-              ['px-1 py-1']: !children,
-              ['text-text-primary bg-main-theme-3']: variant === 'primary',
-              ['text-text-alt2 bg-text-primary']: variant === 'secondary',
-              ['text-text-primary']: variant === 'text',
-            },
-          ),
-        )}
-      >
-        {iconLeft && iconLeft}
-        {children && children}
-        {iconRight && iconRight}
-      </Component>
+      <div className={clsx('overflow-hidden rounded-full', className)}>
+        <Component
+          ref={ref as any}
+          {...(rest as any)}
+          className={twMerge(
+            clsx(
+              'group transition-all duration-200 ui-button rounded-full flex items-center gap-3 outline-offset-2 relative overflow-hidden focus-within:ring-offset-current cursor-pointer',
+              {
+                ['text-sm leading-6 py-[6px] px-4']: size === 'default',
+                ['text-xs px-3 py-1']: size === 'sm',
+                ['px-1 py-1']: !children,
+                ['text-text-primary bg-main-theme-3']: variant === 'primary',
+                ['text-text-alt2 bg-text-primary']: variant === 'secondary',
+                ['text-text-primary']: variant === 'text',
+              },
+            ),
+          )}
+        >
+          {iconLeft && iconLeft}
+          {children && children}
+          {iconRight && iconRight}
+          <div
+            className={clsx(
+              'right-0 pointer-events-none absolute top-0 w-full h-full [.theme-light_&]:mix-blend-color-burn [.theme-dark_&]:mix-blend-color-dodge bg-neutral-500 transition-all duration-300 group-hover:translate-x-0 -translate-x-full',
+            )}
+          />
+        </Component>
+      </div>
     );
   },
 );
