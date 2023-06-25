51b95c98ef7c6a601a99deeb6b0b7f77934bc543
rpavlini <robert.pavlinic95@gmail.com>
2023-06-25 22:55:34 +0200

    media query hook

---
+++ src/hooks/useMatchMedia.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+export default function useMatchMedia(query: string, defaultState?: boolean) {
+  const [state, setState] = useState(() => {
+    if (defaultState !== undefined) {
+      return defaultState;
+    }
+
+    if (typeof window !== 'undefined') {
+      return window.matchMedia(query).matches;
+    }
+
+    return false;
+  });
+
+  useEffect(() => {
+    let mounted = true;
+    const media = window.matchMedia(query);
+    const onChange = () => {
+      if (!mounted) {
+        return;
+      }
+      setState(Boolean(media.matches));
+    };
+
+    setState(media.matches);
+
+    media.addEventListener('change', onChange);
+    return () => {
+      mounted = false;
+      media.removeEventListener('change', onChange);
+    };
+  }, [query]);
+
+  return state;
+}
