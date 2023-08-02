a49d0f8efc5c68cea7d61d93176d437a8b5442a5
rpavlini <robert.pavlinic95@gmail.com>
2023-08-02 02:15:36 +0200

    404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+
+import Button from '../components/ui/Button';
+import Heading from '../components/ui/Heading';
+import Header from './components/Header';
+
 export default function NotFound() {
-  return <h1>404</h1>;
+  return (
+    <>
+      <Header />
+      <main className="flex flex-1 flex-col items-center px-11 py-10">
+        <Heading className="mb-6 text-6xl  md:mb-10 md:text-9xl">404</Heading>
+        <p className="mb-4 md:mb-6">Oops! Page not found.</p>
+        <Button asChild>
+          <Link href="/">Go Back to Home</Link>
+        </Button>
+      </main>
+    </>
+  );
 }
