d9322f4a5f319a9853b7f666a8cac9cc7f874aa4
Marijana Pavlinić <46031252+marijanapav@users.noreply.github.com>
2025-06-28 23:47:52 +0200

    bio card remove socials and cleanup paragraph

--- src/app/components/BioCard.tsx
+++ src/app/components/BioCard.tsx
@@ -25,29 +25,19 @@ const social = [
     attrs: { 'aria-label': 'Go to Twitter' },
   },
   {
+    url: 'https://github.com/marijanapav',
+    Icon: GithubIcon,
+    attrs: { 'aria-label': 'Go to GithHub' },
+  },
+  {
     url: 'https://www.linkedin.com/in/marijana-pavlinic/',
     Icon: LinkedinIcon,
     attrs: { 'aria-label': 'Go to LinkedIn' },
   },
   {
-    url: 'https://www.instagram.com/marijanapavlinic/',
-    Icon: InstagramIcon,
-    attrs: { 'aria-label': 'Go to Instagram' },
-  },
-  {
     url: 'https://bsky.app/profile/marijanapav.com',
     Icon: BlueskyIcon,
     attrs: { 'aria-label': 'Go to Bluesky' },
-  },
-  {
-    url: 'https://read.cv/marijanapav',
-    Icon: ReadCVIcon,
-    attrs: { 'aria-label': 'Go to ReadCV' },
-  },
-  {
-    url: 'https://github.com/marijanapav',
-    Icon: GithubIcon,
-    attrs: { 'aria-label': 'Go to GithHub' },
   },
 ];
 
@@ -64,34 +54,35 @@ export default function BioCard() {
           sizes="(max-width: 768px) 100vw, 768px"
           priority
         />
-        <div className="bg-panel-overlay absolute left-0 top-0 h-full w-full rounded-md transition-colors duration-200" />
+        <div className="absolute left-0 top-0 h-full w-full rounded-md bg-panel-overlay transition-colors duration-200" />
       </div>
 
-      <p className="panel leading-7 text-text-primary">
-        I&apos;m a designer interested in tech, co-running{' '}
-        <TextLink href="https://www.buka.studio">Buka Studio</TextLink>. I have experience working
-        in a variety of environments, from small design studios to a large agency, as well as
-        startups and contracting for various clients. Along the way, I&apos;ve picked up a mix of
-        skills from editorial design, illustration, merchandise, to now primarily focusing on web
-        design, UX and brand design. I&apos;m based in Europe, Croatia, working from my home
-        office—because life&apos;s too short for long commutes, but I do have to deal with a cat
-        trying to sit on my keyboard.
+      <p className="panel text-sm leading-6 text-text-primary">
+        I’m a designer interested in tech, exploring brand, web, illustration, and code. I co-run{' '}
+        <TextLink href="https://www.buka.studio">Buka Studio</TextLink> where I help teams craft
+        cohesive experiences across product, marketing, and everything in between. These days, my
+        focus is on designing brand and web for devtools—which led me to try frontend myself,
+        curious about how implementation can push design further when you follow it all the way from
+        Figma to prod. I&apos;m based in Croatia, working alongside my partner and a cat who rarely
+        skips a stand-up.
       </p>
-      <div className="mt-9 flex flex-col items-start justify-between text-text-primary md:flex-row md:items-center">
-        <div>See what I&apos;ve been doing on:</div>
-        <div className="mr-auto mt-3 flex gap-2 md:ml-5 md:mt-0">
-          {social.map(({ url, Icon, attrs }) => (
-            <a
-              target="_blank"
-              rel="noreferrer noopener"
-              key={url}
-              href={`${url}`}
-              className="hover hoverable:text-theme-2 hoverable:hover:text-theme-1 cursor-pointer rounded-full transition-all duration-200"
-              {...attrs}
-            >
-              <Icon />
-            </a>
-          ))}
+      <div className="mt-4 flex flex-col items-start justify-between text-text-primary md:flex-row md:items-center">
+        <div className="flex flex-wrap items-center gap-2">
+          <span className="text-base">See what I&apos;ve been doing on</span>
+          <div className="flex gap-2">
+            {social.map(({ url, Icon, attrs }) => (
+              <a
+                target="_blank"
+                rel="noreferrer noopener"
+                key={url}
+                href={`${url}`}
+                className="hover cursor-pointer rounded-full transition-all duration-200 hoverable:text-theme-2 hoverable:hover:text-theme-1"
+                {...attrs}
+              >
+                <Icon />
+              </a>
+            ))}
+          </div>
         </div>
         <Button className="mt-10 md:mt-0" iconRight={<ArrowRightIcon />} asChild>
           <Link href="/contact">Let&apos;s talk</Link>
