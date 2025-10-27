e67faf23307204bef6e353a2182e7a5badfcb5f6
rpavlini <robert.pavlinic95@gmail.com>
2025-10-27 19:00:32 +0100

    hooks misc

--- src/hooks/useMatchMedia.ts
+++ src/hooks/useMatchMedia.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 
+// todo: use syncExternalStore
 export default function useMatchMedia(query: string, defaultState?: boolean) {
   const [state, setState] = useState(() => {
     if (defaultState !== undefined) {
--- src/hooks/useResizeRef.ts
+++ src/hooks/useResizeRef.ts
@@ -1,22 +1,33 @@
 import { useLayoutEffect, useRef, useState } from 'react';
 
-export default function useResizeRef<T extends HTMLElement>() {
+export default function useResizeRef<T extends HTMLElement>(once?: boolean) {
   const ref = useRef<T>(null);
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const measured = useRef(false);
 
   useLayoutEffect(() => {
-    if (!ref.current) return;
+    if (!ref.current) {
+      return;
+    }
+
+    if (once && measured.current) {
+      return;
+    }
 
     if (ref.current) {
       setDimensions({
         width: ref.current.clientWidth,
         height: ref.current.clientHeight,
       });
+
+      measured.current = true;
     }
 
     const observer = new ResizeObserver((entries) => {
       const entry = entries[0];
-      if (!entry) return;
+      if (!entry) {
+        return;
+      }
 
       setDimensions({
         width: entry.contentRect.width,
@@ -27,7 +38,7 @@ export default function useResizeRef<T extends HTMLElement>() {
     observer.observe(ref.current);
 
     return () => observer.disconnect();
-  }, []);
+  }, [once]);
 
   return { ref, dimensions };
 }
