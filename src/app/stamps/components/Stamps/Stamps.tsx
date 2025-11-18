c093d39eedcee838d4eb571ef4d45d1f980b607b
rpavlini <robert.pavlinic95@gmail.com>
2025-11-18 21:02:33 +0100

    style tweaks

--- src/app/stamps/components/Stamps/Stamps.tsx
+++ src/app/stamps/components/Stamps/Stamps.tsx
@@ -696,7 +696,7 @@ export default function Stamps({ className, ...props }: ComponentProps<typeof mo
             cellWidth={gridCellSize}
             cellHeight={gridCellSize}
             align="top"
-            className={cn('absolute inset-0 opacity-40 md:opacity-100')}
+            className={cn('absolute inset-0 opacity-40 lg:opacity-100')}
           />
         </div>
         <div
@@ -763,7 +763,7 @@ export default function Stamps({ className, ...props }: ComponentProps<typeof mo
                         custom={{
                           scale: invertScale(centerScale),
                         }}
-                        className="pointer-events-auto absolute top-full flex w-full items-center justify-center gap-5"
+                        className="pointer-events-auto absolute top-full flex w-full items-center justify-center gap-5 lg:top-[calc(100%+8px)]"
                       >
                         <motion.div {...fadeInProps} key="info-button" className="lg:hidden">
                           <Drawer
